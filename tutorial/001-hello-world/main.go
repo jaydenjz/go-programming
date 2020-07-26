572d2878bd5111445b91a5ce03714b6b38cb29bf
Jayden <jzfam93@live.com>
2020-07-26 15:59:45 +0800

    try out loop, if and method

--- tutorial/001-hello-world/main.go
+++ tutorial/001-hello-world/main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+	foo()
+	fmt.Println("Nice!")
+	for i := 0; i < 100; i++ {
+		if i%5 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("This is foo!")
 }
