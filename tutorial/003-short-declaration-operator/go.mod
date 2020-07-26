16e5ad1d9ee55c7dc1e5473766ab7a20c18b0b96
Jayden <jzfam93@live.com>
2020-07-26 16:49:19 +0800

    added code to short description

---
+++ tutorial/003-short-declaration-operator/go.mod
@@ -0,0 +1,3 @@
+module github.com/jaydenjz/go-programming/tutorial/003-short-declaration-operator
+
+go 1.14
---
+++ tutorial/003-short-declaration-operator/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x)
+	y := 123 + 545
+	fmt.Println(y)
+	z := "Chicken rice"
+	fmt.Println(z)
+}
